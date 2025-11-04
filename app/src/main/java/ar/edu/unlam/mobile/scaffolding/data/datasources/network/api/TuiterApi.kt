61b8b2ed3db280ddea84f5092ff0c58b0f8aebe7
FacuCisneros7 <Facundocisneros93@gmail.com>
2025-11-04 17:13:28 -0300

    cambios de TuiterApi

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/datasources/network/api/TuiterApi.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/datasources/network/api/TuiterApi.kt
@@ -3,6 +3,7 @@ package ar.edu.unlam.mobile.scaffolding.data.datasources.network.api
 import ar.edu.unlam.mobile.scaffolding.data.datasources.local.model.LoginRequest
 import ar.edu.unlam.mobile.scaffolding.data.datasources.local.model.Tuit
 import ar.edu.unlam.mobile.scaffolding.data.datasources.local.model.UserApiResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -15,4 +16,7 @@ interface TuiterApi {
     suspend fun logIn(
         @Body request: LoginRequest,
     ): UserApiResponse
+
+    @POST("/api/v1/users")
+    suspend fun register(@Body registerRequest: RegisterRequest): Response<RegisterResponse>
 }
